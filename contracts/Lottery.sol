9df9c18bcbff3ccb502a2e1e8b0588df00724b31
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-10-03 17:58:04 +0530

    added get Players function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -30,6 +30,10 @@ contract Lottery {
         require(msg.sender == manager);
         _;
     }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
     
 
 
