d5074b37987aa04d0565e388ece46c93202eee03
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-10-03 17:43:02 +0530

    fixed compile.js and deploy.js

--- compile.js
+++ compile.js
@@ -2,8 +2,8 @@ const solc = require("solc");
 const path = require("path");
 const fs = require("fs");
 
-const inboxPath = path.join(__dirname, "contracts", "Lottery.sol");
-const source = fs.readFileSync(inboxPath, "utf8");
+const lotteryPath = path.join(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf8");
 
 var input = {
   language: "Solidity",
--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const Web3 = require("web3");
 const { abi, evm } = require("./compile");
 
 const provider = new HDWalletProvider(
-    "write here your 12 word mnemonic",
+    "solution alcohol custom social excess festival desert judge arm coach horror corn",
     "https://rinkeby.infura.io/v3/989fd50fdbe843248288caaf4bab85d8"
 );
 
@@ -14,7 +14,7 @@ const deploy = async () => {
     console.log("Attempting to deploy from account", accounts[0]);
     // console.log(accounts);
     const result = await new web3.eth.Contract(abi)
-        .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+        .deploy({ data: evm.bytecode.object})
         .send({ from: accounts[0], gas: "1000000" });
     console.log("Contract deployed to: ", result.options.address);
     provider.engine.stop();
